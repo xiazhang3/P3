58ba84514c19b5a2751977a9b221d2da32b30e76
Xia Zhang <xianiu3@gmail.com>
2014-07-11 18:52:40 -0400

    should meet the basic requirement

--- app/controllers/GeneratorController.php
+++ app/controllers/GeneratorController.php
@@ -14,12 +14,33 @@ class GeneratorController extends BaseController {
 		return View::make('user');
 	}
 
+	//This function requires "badcow/lorem-ipsum": "dev-master"
 	public function postCreateParagraph(){
-		return 'hello';
+		$paraData = (int)Input::get('num_paragraph');
+
+
+		$generator = new Badcow\LoremIpsum\Generator();
+		$paragraphs = $generator->getParagraphs($paraData);
+
+		//modify the usage accordingly
+		echo '<p>'.implode('</p><p>', $paragraphs).'</p>';
+
 	}
 
+	//This function requires "fzaninotto/faker": "1.5.*@dev"
 	public function postCreateUser() {
-		return 'heloo';
+		$userData = (int)Input::get('num_user');
+
+		// use the factory to create a Faker\Generator instance
+		$faker = Faker\Factory::create();
+		
+		for($i = 0; $i < $userData; $i++) {
+		      echo '<p>'.$faker->name.'</p>';  
+			  echo '<p>'.$faker->address.'</p>'; 
+		      echo '<p>'.$faker->email.'</p>'; 
+		      echo '<br>';
+		}
+
 	}
 
 
